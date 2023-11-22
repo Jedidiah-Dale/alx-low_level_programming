965e56038d4056527ad2610ec75fce961203278d
Jedidiah-Dale <jediditanya@gmail.com>
2023-11-22 14:06:50 -0800

    prepocessor

---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: A program that prints the name of the file it was compiled from
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+
---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description - a program that prints where the file has been compiled from
+ *
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+printf("%s\n", __FILE__);
+return (0);
+}
